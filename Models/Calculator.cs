942d97b2a66aeac1b1643c435fba81374a72105d
Alper Çelik <alper@alper-celik.dev>
2025-06-12 21:53:29 +0300

    refactor calculator logic and enhance UI interactions

--- Models/Calculator.cs
+++ Models/Calculator.cs
@@ -6,6 +6,30 @@ namespace MySampleCalc.Models;
 public static class Calculator
 {
   public static bool IsValidOperation(string operation) => operation != String.Empty;
-  public static bool CouldBeValidOperation(string operation) => operation.Length > 2;
+  public static bool CouldBeValidOperation(string operation, char addition)
+  {
+    if (addition == ')')
+    {
+      return CanAddParenClose(operation);
+    }
+    return true;
+  }
   public static string Calculate(string operation) => "not implemented";
+
+  private static bool CanAddParenClose(string op)
+  {
+    int numParenOpen = 0;
+    foreach (var ch in op)
+    {
+      if (ch == '(')
+      {
+        numParenOpen++;
+      }
+      else if (ch == ')')
+      {
+        numParenOpen--;
+      }
+    }
+    return numParenOpen > 0;
+  }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,15 @@ using ReactiveUI;
 using MySampleCalc.Models;
 using Avalonia.Controls.Platform;
 using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using Tmds.DBus.Protocol;
 
 namespace MySampleCalc.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private string _operation = "Welcome to My Calculator!";
+    private string _operation = String.Empty;
     public string Operation
     {
         get => _operation;
@@ -25,8 +28,12 @@ public partial class MainWindowViewModel : ViewModelBase
 
 
     public ReactiveCommand<Unit, Unit> Calculate { get; }
+    public ReactiveCommand<Unit, Unit> Delete { get; }
+    public ReactiveCommand<Unit, Unit> DeleteAll { get; }
 
     public ReactiveCommand<Unit, Unit>[] EnterNumber { get; }
+    public Dictionary<String, ReactiveCommand<Unit, Unit>> EnterSymbol { get; }
+    public ReactiveCommand<Unit, Unit> EnterDecimal => EnterSymbol["."];
     public MainWindowViewModel()
     {
         IObservable<bool> isOperationValid = this.WhenAnyValue(
@@ -44,10 +51,37 @@ public partial class MainWindowViewModel : ViewModelBase
             int number = i;
             IObservable<bool> couldBeValidNumber = this.WhenAnyValue(
                 x => x.Operation,
-                x => Calculator.CouldBeValidOperation(x + number)
+                x => Calculator.CouldBeValidOperation(x, number.ToString()[0])
             );
             EnterNumber[number] = ReactiveCommand.Create(() => { Operation += number; }, couldBeValidNumber);
         }
+
+        var _enterSymbol = new Dictionary<String, ReactiveCommand<Unit, Unit>>();
+        foreach (var _symbol in new string[] { "+", "-", "/", "*", "(", ")", "." })
+        {
+            var symbol = _symbol;
+            IObservable<bool> couldBeValidSymbol = this.WhenAnyValue(
+             x => x.Operation,
+             x => Calculator.CouldBeValidOperation(x, symbol[0])
+            );
+
+            _enterSymbol.Add(symbol, ReactiveCommand.Create(() => { Operation += symbol; }, couldBeValidSymbol));
+        }
+        EnterSymbol = _enterSymbol;
+
+        IObservable<bool> isOperationNotEmpty = this.WhenAnyValue(
+          x => x.Operation,
+          x => x.Length > 0);
+
+        Delete = ReactiveCommand.Create(
+          void () => Operation = Operation.Substring(0, Operation.Length - 1),
+          isOperationNotEmpty
+        );
+
+        DeleteAll = ReactiveCommand.Create(
+                void () => Operation = String.Empty,
+                isOperationNotEmpty
+            );
     }
 
 
